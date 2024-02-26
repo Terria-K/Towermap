3d0a94d4441342baef1bc17d39d2aa5c980067cd
Terria-K <aelbrian028@gmail.com>
2024-02-26 17:15:03 +0800

    Search text edit for entities

--- Towermap/Core/Layer/Entities.cs
+++ Towermap/Core/Layer/Entities.cs
@@ -8,6 +8,7 @@ public class Entities : ImGuiElement
 {
     private ActorManager manager;
     private IntPtr imGuiTexture;
+    private string searchText = string.Empty;
     public Action<Actor> OnSelectActor;
     public Entities(ActorManager manager, IntPtr imGuiTexture) 
     {
@@ -20,17 +21,22 @@ public class Entities : ImGuiElement
         ImGui.BeginChild("Entities");
         ImGui.SeparatorText("Entities");
 
+        ImGui.InputText("Search", ref searchText, 50);
+
         ImGui.PushItemWidth(-1);
         ImGui.BeginListBox("##Entities", new Vector2(180, 200));
         foreach (var (name, actor) in manager.Actors) 
         {
-            if (ImGui.ImageButton(name, imGuiTexture, new Vector2(20, 20) * 1.5f, 
-                actor.Texture.UV.TopLeft.ToNumericsVec2(), actor.Texture.UV.BottomRight.ToNumericsVec2())) 
+            if (string.IsNullOrEmpty(searchText) || name.ToLowerInvariant().Contains(searchText.ToLowerInvariant())) 
             {
-                OnSelectActor?.Invoke(actor);
+                if (ImGui.ImageButton(name, imGuiTexture, new Vector2(20, 20) * 1.5f, 
+                    actor.Texture.UV.TopLeft.ToNumericsVec2(), actor.Texture.UV.BottomRight.ToNumericsVec2())) 
+                {
+                    OnSelectActor?.Invoke(actor);
+                }
+                ImGui.SameLine();
+                ImGui.Text(name);
             }
-            ImGui.SameLine();
-            ImGui.Text(name);
         }
         ImGui.EndListBox();
         ImGui.PopItemWidth();

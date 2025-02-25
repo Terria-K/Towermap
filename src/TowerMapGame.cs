ec5db0c5642960f536a5fef689ec6b5268d0fab9
Terria-K <teuria@proton.me>
2025-02-25 22:41:04 +0800

    Moved entrypoint to game

--- src/TowerMapGame.cs
+++ src/TowerMapGame.cs
@@ -1,3 +1,4 @@
+using System;
 using Riateu;
 using Riateu.Content;
 using Riateu.Graphics;
@@ -49,5 +50,26 @@ public class TowermapGame : GameApp
             Scene.Render(commandBuffer, swapchainTarget);
         }
         GraphicsDevice.Submit(commandBuffer);
+    }
+
+    [STAThread]
+    private static void Main()
+    {
+        var game = new TowermapGame(new Riateu.WindowSettings("Towermap", 1280, 640, WindowMode.Windowed, new Flags 
+        {
+            Borderless = true
+        }, OnHitTest), Riateu.GraphicsSettings.Debug);
+        game.Run();
+    }
+
+
+    private static HitTestResult OnHitTest(Window window, Point area) 
+    {
+        if (area.Y < 20)
+        {
+            return HitTestResult.Draggable;
+        }
+
+        return HitTestResult.Normal;
     }
 }

bdd575c49a699d830ebf62fa14279d3da6ebc354
Terria-K <teuria@proton.me>
2025-03-07 01:07:14 +0800

    Added Mud

---
+++ src/Entities/Mud.js
@@ -0,0 +1,13 @@
+return {
+    name: "Mud",
+    texture: "mud",
+    width: 10,
+    height: 10,
+    originX: 0,
+    originY: 5,
+    darkWorld: true,
+    resizableX: true,
+    tags: ["Interactable"],
+    textureSize: [10, 10],
+    onRender: "TileRender3x1"
+}

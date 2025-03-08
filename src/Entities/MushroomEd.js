7c33eed91cacc7f3ededddc966b604a79bd52ae1
Terria-K <teuria@proton.me>
2025-03-08 19:58:14 +0800

    Added MushroomEd

---
+++ src/Entities/MushroomEd.js
@@ -0,0 +1,10 @@
+return {
+    name: "MushroomEd",
+    texture: "details/wallMushroom",
+    width: 10,
+    height: 10,
+    originX: 5,
+    originY: 0,
+    tags: ["Details"],
+    textureSize: [10, 10]
+}
